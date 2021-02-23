55d98b391e289e3de8caedbb59ca379dbc07423a
RODRIGO NEGRON <rodrigoo.n@hotmail.com.argit config --global user.email rodrigoo.n@hotmail.com.ar>
2021-02-23 01:50:28 -0300

    bcrypt y middleware de validaciones agregados

--- website/controllers/userController.js
+++ website/controllers/userController.js
@@ -1,9 +1,15 @@
 const User = require("../models/Users");
+const {validationResult} = require ("express-validator");
+const bcryptjs = require("bcryptjs");
 
 const userController ={
 
     login: (req,res)=>{
         res.render("./user/login")
+    },
+    
+    processRegister:(req,res) =>{
+
     },
 
     register:(req,res)=>{
@@ -11,12 +17,24 @@ const userController ={
     },
     
     processRegister:(req,res) =>{
+        
+        let errors = validationResult(req);
+        console.log(errors)
+        
+        if(!errors.isEmpty()){
+            return res.render("./user/register", {
+                errors: errors.mapped(),
+                oldData: req.body
+            });
+        }
+
         let newUser={
             ...req.body,
-           image: req.file.filename
+           image: req.file.filename,
+           password: bcryptjs.hashSync(req.body.password, 10)
         }
         User.create(newUser);
-        return res.redirect("/")
+        return res.redirect("/users/login")
     }
 }
 
--- website/routes/userRoutes.js
+++ website/routes/userRoutes.js
@@ -1,12 +1,16 @@
 const express =require ("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
-const fileUpload = require("../middlewares/multerMiddleware")
+const fileUpload = require("../middlewares/multerMiddleware");
+const userValidation = require("../middlewares/validateMiddleware");
+
 
 router.get("/login", userController.login);
 
+router.post("/login", userValidation ,userController.processLogin);
+
 router.get("/register", userController.register);
 
-router.post("/register", fileUpload.single("image"), userController.processRegister); 
+router.post("/register", fileUpload.single("image"),userValidation, userController.processRegister); 
 
 module.exports=router;
