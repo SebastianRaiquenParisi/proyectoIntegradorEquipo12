4b3f8541307712a748c6c3043c5751c277b433d2
nuevo-rama <ramiromelero@gmail.com>
2021-03-26 01:55:57 -0300

    agrego comentarios en validaciones

--- website/middlewares/validateMiddleware.js
+++ website/middlewares/validateMiddleware.js
@@ -1,21 +1,54 @@
 const {check} = require("express-validator");
 const path = require("path");
 
+//revisar
+
+/*const validateLogin= [
+    //.isEmail()
+    check("email").notEmpty().withMessage("Tienes que escribir un email válido"), 
+    check("password").notEmpty().isLength({ min: 5 }).withMessage
+    ("Tienes que escribir una contraseña con más de 5 caracteres"),
+]
+
+const validateRegister= [
+    check("name").notEmpty().isLength({min:1, max: 100}).withMessage("Debes completar el nombre"),
+    //.isEmail()
+    // validar si el email está en uso
+    check("email").notEmpty().withMessage("Tienes que escribir un email válido"), 
+    check("password").notEmpty().isLength({ min: 5 }).withMessage
+    ("Tienes que escribir una contraseña con más de 5 caracteres"),
+    check("image").withMessage("La imagen debe ser .jpg o .png"),
+]
+
+const validateRecoverLogin= [
+    //.isEmail()
+    check("email").notEmpty().withMessage("Tienes que escribir un email válido"),
+]
+
+//exportar el módulo de validaciones
+//las implementamos en la ruta que procesa el formulario por POST
+
+//implementar validaciones para los formularios de creación y edición de productos
+*/
+
+
+//en funcionamiento?
+
 module.exports=[
     check("name").notEmpty().withMessage("Debes completar el nombre"),
-    check("email").notEmpty().withMessage("tienes que escribir un email valido"),
+    check("email").notEmpty().withMessage("Tienes que escribir un email válido"),
     check("image").custom((value, {req})=> {
         let file=req.file;
         let acceptedExtensions = [".jpg", ".png", ".jpeg", ".gif"];
 
         if(!file){
-            throw new Error("Debes agregar una imagen");
+            throw new Error("Debes agregar una imagen"); //El formato no corresponde a una imágen válida
         }/* else{
             let fileExtension = path.extname(path.originalname);
             if(!acceptedExtensions.includes(fileExtension)){
                 throw new Error ("las extensiones permitidas son ${acceptedExtensions.join}");
             }
         } */
-        return true;
+        return true; //por qué retorna en true??
     }) 
 ]
