0d104b166340bdfe4fc79370b7b3e7eda20e0a15
nuevo-rama <ramiromelero@gmail.com>
2021-04-30 18:14:52 -0300

    validaciones del front

--- website/public/js/validationCreateFront.js
+++ website/public/js/validationCreateFront.js
@@ -1,8 +1,8 @@
 window.addEventListener("load", function(){
 
-    let form= document.querySelector("form.m-auto col-md-7"); //CAPTURO EL FORMULARIO COMPLETO
+    let form= document.querySelector("form.formCreate"); //CAPTURO EL FORMULARIO COMPLETO
      
-    let inputImage= document.querySelector("input.form-control m-auto"); //CAPTURO EL INPUT IMAGE
+    let inputImage= document.querySelector("input#image_url"); //CAPTURO EL INPUT IMAGE
     let inputName= document.querySelector("input#name"); //CAPTURO EL INPUT NAME
     let textareaDescription= document.querySelector("textarea#description"); //CAPTURO EL INPUT DESCRIPTION 
     let textareaFullDescription= document.querySelector("textarea#fullDescription"); //CAPTURO EL INPUT FULLDESCRIPTION
@@ -13,18 +13,16 @@ window.addEventListener("load", function(){
     let errorsListDescription= document.querySelector("#errorsListDescription");
     let errorsListFullDescription= document.querySelector("#errorsListFullDescription");
     
+    let errorCounter="";
     
-    
-    form.addEventListener("submit", function(e){
-
-        let hasErrors= false; //COMIENZA SIN ERRORES
-        e.preventDefault(); 
+    form.addEventListener("keypress", function(){
+        var hasErrors= false; //COMIENZA SIN ERRORES
 
         errorsListImage.innerHTML="";
         errorsListName.innerHTML="";
         errorsListDescription.innerHTML="";
         errorsListFullDescription.innerHTML="";
-
+    
         if (inputImage.value=="") {
             hasErrors= true;
             errorsListImage.innerHTML += "<li> Debes ingresar una imagen </li>";
@@ -33,7 +31,7 @@ window.addEventListener("load", function(){
             hasErrors= true;   
             errorsListName.innerHTML += "<li> El campo Título no puede estar vacío </li>";
             }
-        if (inputName.value.length<5) {
+        else if (inputName.value.length<5) {
             hasErrors= true;   
             errorsListName.innerHTML += "<li> El campo Título debe tener como mínimo 5 caracteres</li>";
             }
@@ -41,7 +39,7 @@ window.addEventListener("load", function(){
             hasErrors= true;
             errorsListDescription.innerHTML += "<li> El campo Subtítulo no puede estar vacío </li>";
             }
-        if (textareaDescription.value.length<3) {
+        else if (textareaDescription.value.length<3) {
             hasErrors= true;
             errorsListDescription.innerHTML += "<li> El campo Subtítulo debe tener como mínimo 3 caracteres </li>";
             }
@@ -49,50 +47,22 @@ window.addEventListener("load", function(){
             hasErrors= true;
             errorsListFullDescription.innerHTML += "<li> La descripción no puede estar vacía </li>";
             }
-        if (textareaFullDescription.value.length<20) {
+        else if (textareaFullDescription.value.length<20) {
             hasErrors= true;
             errorsListFullDescription.innerHTML += "<li> La descripción debe tener como mínimo 20 caracteres </li>";
             }
-
-        if(!hasErrors) { //SI NO HAY ERRORES SE EJECUTA EL SUBMIT
-            this.submit(); 
-        }
+        
+        if(hasErrors==false) {
+            errorCounter="sin errores";
+            }
     });
 
-    form.addEventListener("keypress", function(){
+    form.addEventListener("submit", function(e){
 
-        errorsListImage.innerHTML="";
-        errorsListName.innerHTML="";
-        errorsListDescription.innerHTML="";
-        errorsListFullDescription.innerHTML="";
-    
-        if (inputImage.value=="") {
-            hasErrors= true;
-            errorsListImage.innerHTML += "<li> Debes ingresar una imagen </li>";
-            }
-        if (inputName.value=="") {
-            hasErrors= true;   
-            errorsListName.innerHTML += "<li> El campo Título no puede estar vacío </li>";
-            }
-        if (inputName.value.length<5) {
-            hasErrors= true;   
-            errorsListName.innerHTML += "<li> El campo Título debe tener como mínimo 5 caracteres</li>";
-            }
-        if (textareaDescription.value=="") {
-            hasErrors= true;
-            errorsListDescription.innerHTML += "<li> El campo Subtítulo no puede estar vacío </li>";
-            }
-        if (textareaDescription.value.length<3) {
-            hasErrors= true;
-            errorsListDescription.innerHTML += "<li> El campo Subtítulo debe tener como mínimo 3 caracteres </li>";
-            }
-        if (textareaFullDescription.value=="") {
-            hasErrors= true;
-            errorsListFullDescription.innerHTML += "<li> La descripción no puede estar vacía </li>";
-            }
-        if (textareaFullDescription.value.length<20) {
-            hasErrors= true;
-            errorsListFullDescription.innerHTML += "<li> La descripción debe tener como mínimo 20 caracteres </li>";
-            }
-    })
+        if(errorCounter=="sin errores") { //SI NO HAY ERRORES SE EJECUTA EL SUBMIT
+            this.submit(); 
+        } else {
+            e.preventDefault();
+        }
+    });
 })
