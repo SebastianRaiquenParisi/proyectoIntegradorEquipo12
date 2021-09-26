68c2ab3adf684c2cb05d5fdf7f9d3e50e76c555e
sebastianRaiquenParisi <sebastianrparisi@gmail.com>
2021-09-25 21:10:15 -0300

    heroku hosted website

--- website/app.js
+++ website/app.js
@@ -13,8 +13,9 @@ app.use(cookies());
 app.use(loggedUserMiddleware);
 app.use(methodOverride('_method'));
 
-app.listen (3000, () =>
-console.log ("Servidor corriendo en puerto 3000")
+const PORT = process.env.PORT || 3000;
+app.listen (PORT, () =>
+console.log ("Servidor corriendo en puerto " + PORT)
 );
 
 app.set("views", path.join(__dirname, "views"));
